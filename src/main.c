a3928be0b46638708d9ebf4ef7b7f3dcff94faed
olverhijnzoon <olver.hijnzoon@gmail.com>
2025-01-02 15:48:30 +0100

    Cleanup

--- src/main.c
+++ src/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// function to create an ICS calendar file
 int createICSFile() {
-	
-	// get ICS file output directory from environment variable
 	char *outputDir = getenv("ICS_OUTPUT_DIR");
 	if (outputDir == NULL) {
 		fprintf(stderr, "[ERROR] ICS_OUTPUT_DIR environment variable not set.\n");
@@ -14,20 +11,14 @@ int createICSFile() {
 	return 0;
 }
 	
-// function to write to an ICS calendar file
 int writeICSFile() {
-	printf("[INFO] Writing ICS file\n");
 	return 0;	
 }
 
 int main() {
-	printf("$$$$$$$$$$$$$$\n");
-	printf("[INFO] Calendar Event Creator routine is started.\n");
-	
 	if (createICSFile() != 0){
-					return 1;
+		return 1;
 	}
-
 	writeICSFile();
 	return 0;
 }
