473c42f9397bfe9329cfee5fbaa9e53f15451661
olverhijnzoon <olver.hijnzoon@gmail.com>
2025-01-02 16:05:47 +0100

    Lint

--- src/main.c
+++ src/main.c
@@ -1,24 +1,27 @@
+// Copyright 2025 Oliver Heinsohn
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int createICSFile() {
-	char *outputDir = getenv("ICS_OUTPUT_DIR");
-	if (outputDir == NULL) {
-		fprintf(stderr, "[ERROR] ICS_OUTPUT_DIR environment variable not set.\n");
-		return 1;
-	}
-	printf("[INFO] Environment variable ICS_OUTPUT_DIR is set to '%s'.\n", outputDir);
-	return 0;
+  char *outputDir = getenv("ICS_OUTPUT_DIR");
+  if (outputDir == NULL) {
+    fprintf(stderr, "[ERROR] ICS_OUTPUT_DIR environment variable not set.\n");
+    return 1;
+  }
+  printf("[INFO] Environment variable ICS_OUTPUT_DIR is set to '%s'.\n",
+         outputDir);
+  return 0;
 }
-	
+
 int writeICSFile() {
-	return 0;	
+  return 0;
 }
 
 int main() {
-	if (createICSFile() != 0){
-		return 1;
-	}
-	writeICSFile();
-	return 0;
+  if (createICSFile() != 0){
+    return 1;
+  }
+  writeICSFile();
+  return 0;
 }
