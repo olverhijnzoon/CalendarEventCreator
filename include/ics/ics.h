8a4b0926299d91f9b464a110b430fbdcfc346152
olverhijnzoon <olver.hijnzoon@gmail.com>
2025-01-02 23:03:21 +0100

    Lint

--- include/ics/ics.h
+++ include/ics/ics.h
@@ -1,7 +1,7 @@
 // Copyright 2025 Oliver Heinsohn
 
-#ifndef INCLUDE_ICS_ICS_H
-#define INCLUDE_ICS_ICS_H
+#ifndef INCLUDE_ICS_ICS_H_
+#define INCLUDE_ICS_ICS_H_
 
 #include <stdio.h>
 
@@ -19,4 +19,4 @@ int createICSFile(Event event);
 
 int writeICSFile();
 
-#endif
+#endif // INCLUDE_ICS_ICS_H_
