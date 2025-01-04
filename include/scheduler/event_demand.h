dae4f74a69098d560e317144bf440d8143e6087e
olverhijnzoon <olver.hijnzoon@gmail.com>
2025-01-04 21:14:52 +0100

    format demand event

--- include/scheduler/event_demand.h
+++ include/scheduler/event_demand.h
@@ -3,14 +3,14 @@
 
 typedef struct {
   char start[16];
-	char end[16];
+  char end[16];
 } TimeSlot;
 
 typedef struct {
   char summary[256];
   char description[1024];
-	int durationMinutes;
-	TimeSlot timeSlot;
+  int durationMinutes;
+  TimeSlot timeSlot;
 } EventDemand;
 
 #endif // EVENT_DEMAND_H
--- src/main.c
+++ src/main.c
@@ -5,14 +5,10 @@
 
 int main() {
   EventDemand demand0 = {
-    .summary = "Way to Work",
-    .description = "Plan time to get to workplace.",
-    .durationMinutes = 60,
-    .timeSlot = {
-      .start = "20250106T070000Z",
-      .end = "20250106T080000Z"
-    }
-  };
+      .summary = "Way to Work",
+      .description = "Plan time to get to workplace.",
+      .durationMinutes = 60,
+      .timeSlot = {.start = "20250106T070000Z", .end = "20250106T080000Z"}};
 
   Event dummyEvent = createEvent("20250103T090000Z", "20250103T093000Z",
                                  "Breakfast", "Let's have breakfast.");
